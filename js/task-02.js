59c64b40bfbc3fc0f2d3760797fa91768ab2c445
Kaizer Maksym <kaizeriimaxim@gmail.com>
2022-05-14 00:41:53 +0200

    home-work

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const createIngredientsElements = (elements) => {
+  return elements.map((element) => {
+    const navEl = document.createElement("li");
+    navEl.classList = "item";
+    navEl.textContent = element;
+    return navEl;
+  });
+};
+
+const ingredientsEl = createIngredientsElements(ingredients);
+
+ingredientsList.append(...ingredientsEl);
+
+// console.log(ingredientsEl);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,46 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// const refs = {
+//   input: document.querySelector('input'),
+//   createboxBtn: document.querySelector('[data-create]'),
+//   deleteboxBtn: document.querySelector('[data-destroy]'),
+//   boxes: document.getElementById('boxes'),
+// };
+
+// refs.input.addEventListener('input', inputChange);
+// refs.createboxBtn.addEventListener('click', createBox);
+// refs.deleteboxBtn.addEventListener('click', deleteBox);
+
+// let baseboxSize = 30;
+
+// function inputChange(event) {
+//   event.target.value;
+// }
+
+// function createBox(amount) {
+//   let amount = refs.input.value;
+//   console.log(amount);
+//   for (let i = 0; i < countBox; i++) {
+//     baseboxSize += 10;
+//     const newBox = document.createElement('div');
+//     newBox.style.background = getRandomHexColor();
+//     newBox.style.height = baseboxSize + 'px';
+//     newBox.style.width = baseboxSize + 'px';
+//     newBox.style.margin = '10px';
+//     newBox.classList.add('new-box');
+//     refs.boxes.append(newBox);
+//   }
+// }
+
+// function deleteBox() {
+//   const allNewBoxes = document.querySelectorAll('.new-box');
+//   for (let allNewBoxe of allNewBoxes) {
+//     boxes.removeChild(allNewBoxe);
+//   }
+//   baseboxSize = 30;
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// getRandomHexColor();
